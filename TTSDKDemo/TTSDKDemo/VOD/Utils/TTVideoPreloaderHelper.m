85eb529fbbcdb932abfdc590b7c779c06b45a5da
litianhao.timhao <litianhao.timhao@bytedance.com>
2020-11-18 15:25:31 +0800

    other : 提交VOD修改

--- TTSDKDemo/TTSDKDemo/VOD/Utils/TTVideoPreloaderHelper.m
+++ TTSDKDemo/TTSDKDemo/VOD/Utils/TTVideoPreloaderHelper.m
@@ -26,13 +26,11 @@
             [TTVideoEngine ls_addTask:url.md5String vid:vid preSize:s_preload_size url:url];
         } else if(tt_valid_string(vid)) {
             TTVideoEnginePreloaderVidItem *vidItem = [TTVideoEnginePreloaderVidItem preloaderVidItem:vid
-                                                                                            token:item[SourceKeyAuth]
-                                                                                        reslution:TTVideoEngineResolutionTypeFullHD
-                                                                                      preloadSize:s_preload_size
+                                                                                               token:item[SourceKeyAuth]
+                                                                                           reslution:TTVideoEngineResolutionTypeFullHD
+                                                                                         preloadSize:s_preload_size
                                                                                            isByteVC1:NO];
-            NSString *key = [NSString stringWithFormat:@"vid%@_resolution%@_h265%@_dash%@",vid,@(TTVideoEngineResolutionTypeFullHD),@(NO),@(NO)];
-            [TTVideoEngine ls_addTask:key vidItem:vidItem];
-            // [TTVideoEngine ls_addTaskWithVidItem:vidItem]；/// 建议使用该接口，但是没有自定义 key ,无法取消单个还未进行的 task,但是一般情况下会触发取消之前的所有预加载任务。
+             [TTVideoEngine ls_addTaskWithVidItem:vidItem]；/// 建议使用该接口，但是没有自定义 key ,无法取消单个还未进行的 task,但是一般情况下会触发取消之前的所有预加载任务。
         }
     }
 }
