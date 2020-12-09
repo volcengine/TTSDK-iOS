5d5ac5e46bba79a578bfd85d457a664b19109c18
fengxianghe <fengxianghe@bytedance.com>
2020-12-09 11:49:30 +0800

    <ttuploader>(fix):修复冲突

--- TTSDKDemo/TTSDKDemo/AppDelegate.m
+++ TTSDKDemo/TTSDKDemo/AppDelegate.m
@@ -31,12 +31,8 @@ void uncaughtExceptionHandler(NSException*exception){
     NSSetUncaughtExceptionHandler(&uncaughtExceptionHandler);
     
     NSLog(@"TTSDK version: %@", TTSDKManager.SDKVersionString);
-<<<<<<< HEAD
-
-=======
     // 配置 demo环境变量 模拟国内 海外测试环境
     [TTDemoSDKEnvironmentManager shareEvnironment].serviceVendor = TTSDKServiceVendorCN;
->>>>>>> fxh/dev
     [self initAppLog];
     [self initBDImageManager];
 
