aff867cd35ac28c786d93ef2a67556a0422b409d
chenzhaojie-bd <chenzhaojie-bd@bytedance.com>
2021-11-10 17:04:44 +0800

    others: Update import rangers applog

--- TTSDKDemo/TTSDKDemo/AppDelegate.m
+++ TTSDKDemo/TTSDKDemo/AppDelegate.m
@@ -11,6 +11,9 @@
 #import "NavigationViewController.h"
 #import "HomeViewController.h"
 #import <RangersAppLog/RangersAppLogCore.h>
+#import <RangersAppLog/RangersAppLog.h>
+#import <RangersAppLog/BDAutoTrackURLHostItemCN.h>
+#import <RangersAppLog/BDAutoTrackURLHostItemVA.h>
 #import "TTDemoSDKEnvironmentManager.h"
 #import "LicenseAssociatedConst.h"
 
