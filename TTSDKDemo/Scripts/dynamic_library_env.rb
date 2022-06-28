ea16f973d6ec57710841eab326dd687b4e2417a0
shangjincheng <shangjincheng@bytedance.com>
2022-06-28 13:51:02 +0800

    others: Ommited vendored frameworks

--- TTSDKDemo/Scripts/dynamic_library_env.rb
+++ TTSDKDemo/Scripts/dynamic_library_env.rb
@@ -20,85 +20,60 @@ EOF
 if command == "help" or command.nil?
   puts usage
 else
-  display_name = "TTSDKFramework.framework"
-  display_image_name = "TTSDKImageFramework.framework"
-  byteaudio_name = "byteaudio.framework"
+  file_reference_list = [
+    "TTSDKFramework.framework",
+    "TTSDKImageFramework.framework",
+    "byteaudio.framework",
+    "VolcEngineRTC.framework",
+    "TTFFmpeg.framework",
+    "boringssl.framework",
+    "crypto.framework",
+    "libvcn.framework",
+    "ffmpeg_dashdec.framework",
+    "effect-sdk.framework",
+  ]
+
   #
   IO.readlines(File.join(scripts_dir, "../../TTSDK.podspec")).each { |line| version = line.chop.split("=")[-1].strip[1...-1] if line =~ /spec\.version\s*\=\s*\".*\"/ } if version.nil?
   lib_download_url = "http://sf1-hscdn-tos.pstatp.com/obj/cloud-common/ttsdk/iOS/TTSDKFramework-#{version}-ta.zip"
+
   project_path = File.join(scripts_dir, "../TTSDKDemo.xcodeproj")
   # project setup
   project = Xcodeproj::Project.open(project_path)
   target = project.targets.select { |target| target.name == "TTSDKDemo" }.first
   group = project.groups.select { |group| group.display_name == "extern" }.first
 
-  file_reference = project.objects.select { |object| object.display_name == display_name and object.instance_of? PBXFileReference }.first
-  image_file_reference = project.objects.select { |object| object.display_name == display_image_name and object.instance_of? PBXFileReference }.first
-  byteaudio_file_reference = project.objects.select { |object| object.display_name == byteaudio_name and object.instance_of? PBXFileReference }.first
-  ##
-  build_file = project.objects.select { |object| object.display_name == display_name and object.instance_of? PBXBuildFile }.first
-  image_build_file = project.objects.select { |object| object.display_name == display_image_name and object.instance_of? PBXBuildFile }.first
-  byteaudio_build_file = project.objects.select { |object| object.display_name == byteaudio_name and object.instance_of? PBXBuildFile }.first
-
-  copy_files_build_phases = target.copy_files_build_phases.select { |build_phase| build_phase.name == "Embed Frameworks" }.first
-
-  if command == "setup"
-    puts "dynamic library environment will be setup..."
-    if file_reference.nil?
-      file_reference = group.new_reference(display_name)
-      image_file_reference = group.new_reference(display_image_name)
-      byteaudio_file_reference = group.new_reference(byteaudio_name)
-    end
-  
-    if build_file.nil?
-      build_file = copy_files_build_phases.add_file_reference(file_reference)
-      build_file.settings = {
-        "ATTRIBUTES" => [ 
-          :CodeSignOnCopy, 
-          :RemoveHeadersOnCopy,
-        ]
-      }
-      #
-      image_build_file = copy_files_build_phases.add_file_reference(image_file_reference)
-      image_build_file.settings = {
-        "ATTRIBUTES" => [ 
-          :CodeSignOnCopy, 
-          :RemoveHeadersOnCopy,
-        ]
-      }
-      #
-      byteaudio_build_file = copy_files_build_phases.add_file_reference(byteaudio_file_reference)
-      byteaudio_build_file.settings = {
-        "ATTRIBUTES" => [
-          :CodeSignOnCopy,
-          :RemoveHeadersOnCopy,
-        ]
-      }
-    end
-    unless File.exist?(file_reference.real_path)
-      # Download Framework
-      system <<-EOF
-        echo "start to download library from #{lib_download_url}..."
-        curl #{lib_download_url} -o #{file_reference.real_path}.zip
-        unzip -o -d #{group.real_path} #{file_reference.real_path}.zip
-        rm -rf #{file_reference.real_path}.zip
-      EOF
-    end
-    puts "dynamic library environment did setup..."
-  elsif command == "clean"
-    puts "begin to clean dynamic library environment..."
-    unless file_reference.nil?
-      FileUtils.rm_rf(file_reference.real_path)
-      #
-      copy_files_build_phases.remove_file_reference(file_reference)
-      copy_files_build_phases.remove_file_reference(image_file_reference)
-      copy_files_build_phases.remove_file_reference(byteaudio_file_reference)
-      file_reference.remove_from_project()
-      image_file_reference.remove_from_project()
-      byteaudio_file_reference.remove_from_project()
-    end
-    puts "cleaning dynamic library environment finished..."
+  unless Dir.exist?(File.join(group.real_path, "TTSDKFramework.framework"))
+    # Download Framework
+    system <<-EOF
+      echo "start to download library from #{lib_download_url}..."
+      curl #{lib_download_url} -o #{group.real_path}/TTSDKFramework-ta.zip
+      unzip -o -d #{group.real_path} #{group.real_path}/TTSDKFramework-ta.zip
+      rm -rf #{group.real_path}/TTSDKFramework-ta.zip
+    EOF
   end
 
+  copy_files_build_phases = target.copy_files_build_phases.select { |build_phase| build_phase.name == "Embed Frameworks" }.first
+  file_reference_list.each { |reference|
+    if command == "setup"
+      r = group.new_reference(reference)
+      if Dir.exist?(r.real_path)
+        puts "Add #{reference}"
+        build_file = copy_files_build_phases.add_file_reference(r)
+        build_file.settings = {
+          "ATTRIBUTES" => [ 
+            :CodeSignOnCopy, 
+            :RemoveHeadersOnCopy,
+          ]
+        }
+      end
+    elsif command == "clean" 
+      unless reference.nil?
+        FileUtils.rm_rf(reference.real_path)
+        copy_files_build_phases.remove_file_reference(reference)
+        reference.remove_from_project()
+      end
+    end
+  }
   project.save
 end
